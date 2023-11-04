21ac8bd9cf390d22a5456a49797cbe52413fbe7a
B A G H E R I <B A G H E R I@SALAH>
2023-11-04 15:16:00 +0330

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	float a, b, mesahat,mohit;
+	
+	cout << "enter a:";
+	cin >> a;
+	cout << "enter b:";
+	cin >> b;
+
+	mohit = 2 * (a + b);
+	mesahat = a * b;
+
+	cout << "mohit:" << mohit << endl;
+    cout << "mesahat:" << mesahat;
+
+}
